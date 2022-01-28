a7d98a993d198a8d55a2897889a66436b947c002
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-01-28 10:02:58 -0800

    correct bugs in models

--- bigquery_project/models/mart/customer_order_history.sql
+++ bigquery_project/models/mart/customer_order_history.sql
@@ -12,8 +12,8 @@ with customers as (
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 

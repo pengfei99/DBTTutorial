01b514b9719491801da8bab223ec1d7b1277f2b8
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-01-30 05:04:37 -0800

    update the states

--- postgres_project/models/staging/stg_states.sql
+++ postgres_project/models/staging/stg_states.sql
@@ -3,11 +3,11 @@ with source as (
     select *
     from {{ source('data_mart', 'states') }}
 ),
-renamed as (
-    select state_id::INT AS st_ide,
+stg_states as (
+    select state_id::INT AS st_id,
         state_code::VARCHAR(2) AS st_code,
         state_name::VARCHAR(30) AS st_name
     from source
 )
 select *
-from renamed
+from stg_states

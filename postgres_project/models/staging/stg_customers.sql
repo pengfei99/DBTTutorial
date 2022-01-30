4b49dd384ffa0a66f582c3231b6c683d9b9df426
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-01-30 05:04:05 -0800

    use the snapshot customer table as source

--- postgres_project/models/staging/stg_customers.sql
+++ postgres_project/models/staging/stg_customers.sql
@@ -1,7 +1,16 @@
-with stg_customers as (
+with source as (
     select *
-    from {{ source('data_mart', 'customers') }}
+    from {{ ref('customers_snapshot') }}
+), stg_customers as (
+    select customer_id,
+        zipcode,
+        city,
+        state_code,
+        creation_date::TIMESTAMP AS datetime_created,
+        update_date::TIMESTAMP AS datetime_updated,
+        dbt_valid_from,
+        dbt_valid_to
+    from source
 )
-
 select *
 from stg_customers
